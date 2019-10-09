36206898c65dbf2d187d47e0df513158bf3fdeb6
debuzz <eetherion@gmail.com>
2019-10-09 18:44:33 +0700

    ch

--- TransportService/DeliveryOrderService.svc.cs
+++ TransportService/DeliveryOrderService.svc.cs
@@ -23,6 +23,7 @@ namespace TransportService
     {
         public Stream GetJson(Core.Model.mdlParam json)
         {
+            string chris = "chris";
             var result = new Core.Model.mdlResultSvc();
             result.Title = "Get Json Download";
             var resultJson = new Core.Model.mdlJsonList();
